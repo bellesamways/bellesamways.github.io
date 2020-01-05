783e41fab130ed030b567ef80274274a52dd434d
Isabelle Samways <belesamways@gmail.com>
2020-01-05 17:58:02 -0300

    trocando o icone do botão hamburguer

--- js/main.js
+++ js/main.js
@@ -30,12 +30,12 @@ $(document).ready(function () {
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('hidden animated fadeIn')
   })
 
   $('.navigation-wrapper .blog-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('hidden animated fadeIn')
   })
 
 })
